b9c9f9a416547db7ee26cb42bbe2f8a5b270dfb8
Pranav Joy <pranavjoy@gmail.com>
2020-05-14 08:49:49 +0200

    added post and get

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,12 @@ import hashlib
 from flask import Flask, flash, request, redirect, render_template
 
 
-@app.route('/api/v1/<param>/<user_id>', methods=['POST'])
+@app.route('/api/v1/<param>/<user_id>', methods=['GET', 'POST'])
 def admin(param, user_id):
-    json = {'User_ID': param, 'Response': user_id}
+    if request.method == 'POST':
+        req_in = request.form['request']
+        json = {'Response': 'This is a sample response', 'User_ID': user_id}
+    elif request.method == 'GET':
+        json = {'Response': 'This is a sample response', 'User_ID': user_id}
+        print('GET')
     return make_response(json)
